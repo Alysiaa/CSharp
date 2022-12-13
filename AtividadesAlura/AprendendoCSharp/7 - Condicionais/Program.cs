9030e95f8f27b59b2529fd824bd9bd731203249f
Alysiaa <carol_lopees@outlook.com>
2022-12-12 22:05:06 -0300

    Adicionando projeto 7

---
+++ AtividadesAlura/AprendendoCSharp/7 - Condicionais/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+}
