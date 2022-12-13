0e906da28fcd79fc691fde1f0dc4a0689268fc00
Alysiaa <carol_lopees@outlook.com>
2022-12-12 22:43:44 -0300

    Adicionando Projeto 8

---
+++ AtividadesAlura/AprendendoCSharp/8- Condicionais 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 8 - Condicionais 2");
+
+        int idadeAna = 16;
+        int acompanhante = 3;
+
+        Console.WriteLine(idadeAna);
+
+        // colocando a condição "ou" para diminuir o código = ||
+        // condição "e" = &&
+
+        bool acompanhado = acompanhante > 2;
+        bool grupo = true;
+
+        if (idadeAna >= 18 || acompanhante > 2)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode acessar!");
+        }
+
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+
+}
