00acb3aaf2b2fa20d7725ed9b4a6efb80bbcc582
Alysiaa <carol_lopees@outlook.com>
2022-12-24 12:34:41 -0300

    P14

---
+++ AtividadesAlura/AprendendoCSharp/P14 - Fatorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 14 - Fatorial");
+        int fatorial = 1;
+        for (int i = 1; i < 11; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+        }
+    }
+}
