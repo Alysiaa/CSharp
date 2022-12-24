aaedd3a5a041d3b7740076274c5b4be7749f6c75
Alysiaa <carol_lopees@outlook.com>
2022-12-24 11:51:36 -0300

    Projeto 12

--- AtividadesAlura/AprendendoCSharp/P12 - Investimento/Program.cs
+++ AtividadesAlura/AprendendoCSharp/P12 - Investimento/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 12 - Investimento");
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+}
