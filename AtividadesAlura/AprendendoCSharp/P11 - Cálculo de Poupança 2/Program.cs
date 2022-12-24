149c36678d5437769c1d0cafdb458eccf321daa6
Alysiaa <carol_lopees@outlook.com>
2022-12-24 11:23:36 -0300

    Projeto 11

---
+++ AtividadesAlura/AprendendoCSharp/P11 - Cálculo de Poupança 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 11 - Cálculo de Poupança 2");
+        /*
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você terá: R$ " + investimento);
+
+            mes = mes + 1;
+            // faz a somatória da mesma forma
+            // mes += 1
+            // mes ++
+
+        }
+*/
+
+        double investimento = 1000;
+
+        // rendimento de 0.50% ao mês = 0.005
+        for (int mes = 1; mes <= 12; mes++)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você terá: R$ " + investimento);
+        }
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+}
