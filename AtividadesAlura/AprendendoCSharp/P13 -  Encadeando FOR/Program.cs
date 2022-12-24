a0e86cc6eb1b051737f2d8553cb364f1e68aa6b7
Alysiaa <carol_lopees@outlook.com>
2022-12-24 12:30:24 -0300

    Projeto 13

---
+++ AtividadesAlura/AprendendoCSharp/P13 -  Encadeando FOR/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Encadeando FOR");
+
+        /*
+          *
+          **
+          ***
+          ****
+          *****
+          */
+
+        for (int contlinhas = 0; contlinhas <= 9; contlinhas++)
+        {
+            Console.WriteLine();
+            for (int contcolunas = 0; contcolunas <= 9; contcolunas++)
+            {
+                Console.Write("*");
+                if (contcolunas >= contlinhas)
+                    break;
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+}
